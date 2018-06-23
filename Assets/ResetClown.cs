7b280ea56ddc6080664cfc1425e37b827171a55f
rsowell <rsowell@gmail.com>
2018-06-23 16:23:29 -0500

    initial cake

---
+++ Assets/ResetClown.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetClown : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void ResetClownOnHit () {
+		gameObject.transform.localScale = Vector3.one;
+		gameObject.GetComponentInChildren<CollisionBehavior> ().collided = false;
+	}
+}
